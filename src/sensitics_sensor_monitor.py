c17ac0e9ee797e2f3cc7163fcf9dda811b695abe
Paul Kim <pkim@mbta.com>
2022-10-04 13:57:27 -0400

    Add actions

--- src/sensitics_sensor_monitor.py
+++ src/sensitics_sensor_monitor.py
@@ -1,17 +1,12 @@
-from asyncio.log import logger
-import json
 import logging
 import os
 import requests
 from splunk_hec_handler import SplunkHecHandler
 
-logging.getLogger("sensitics_logger")
-
 def lambda_handler(event, context):
   print("Getting diagnostics...")
-  url = os.environ['SENSITICS_URL']
-  response = requests.get(url)
-  print(response.json())
+  # url = os.environ['SENSITICS_URL']
+  # response = requests.get(url)
 
 if __name__ == "__main__":
     """
