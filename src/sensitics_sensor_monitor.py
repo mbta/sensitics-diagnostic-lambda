55cd1c7d3d4dd487f20c237c986378a107c1dd3f
Paul Kim <pkim@mbta.com>
2022-10-07 08:34:29 -0400

    Add CI and make logger global

--- src/sensitics_sensor_monitor.py
+++ src/sensitics_sensor_monitor.py
@@ -1,69 +1,17 @@
-import boto3
-from botocore.exceptions import ClientError
 import time
 import logging
 import os
 import sys
 import requests
+import boto3
+from botocore.exceptions import ClientError
 from splunk_hec_handler import SplunkHecHandler
 
 
-def lambda_handler(event, context):
-    logger = get_logger()
-    print("Getting diagnostics...")
-
-    try:
-        start = time.time()
-        response = requests.get(os.environ["SENSITICS_URL"], timeout=5)
-        end = time.time()
-        request_log = {'message' : 'Sensitics RTT', 'RTT' : end - start }
-        logger.debug(request_log)
-        response.raise_for_status()
-        log_sensor_statuses(response.json()["sensors"], logger)
-    except requests.exceptions.HTTPError as errh:
-        logger.error("Http Error:", errh)
-        sys.exit()
-    except requests.exceptions.ConnectionError as errc:
-        logger.error("Error Connecting:", errc)
-        sys.exit()
-    except requests.exceptions.Timeout as errt:
-        logger.error("Timeout Error:", errt)
-        sys.exit()
-    except requests.exceptions.RequestException as err:
-        logger.error("Exception:", err)
-        sys.exit()
-
-
-def log_sensor_statuses(sensors, logger):
-    print("Logging to Splunk...")
-    start = time.time()
-    for sensor_status in sensors:
-        sensor_status['message'] = 'Sensor info'
-        logger.info(sensor_status)
-    end = time.time()
-
-    logger.debug(f'Logged to splunk in {end - start} seconds')
-
-
-def get_logger():
-    splunk_hec_host = os.environ["SPLUNK_HEC_HOST"]
-    hec_token_secret_name = os.environ["HEC_TOKEN_AWS_SECRET_NAME"]
-    splunk_index = os.environ["SPLUNK_INDEX"]
-    splunk_handler = SplunkHecHandler(
-        splunk_hec_host,
-        get_splunk_token(hec_token_secret_name),
-        index=splunk_index,
-        port=443, 
-        proto='https'
-    )
-    logger = logging.getLogger("SplunkHecHandler")
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(splunk_handler)
-
-    return logger
-
-
 def get_splunk_token(secret_name):
+    """
+    Helper method to fetch secrets from AWS Secrets Manager
+    """
     secretsmanager = boto3.client("secretsmanager")
 
     try:
@@ -74,6 +22,61 @@ def get_splunk_token(secret_name):
         sys.exit()
     else:
         return secret_value["SecretString"]
+
+
+# Configure logger
+splunk_handler = SplunkHecHandler(
+    os.environ["SPLUNK_HEC_HOST"],
+    get_splunk_token(os.environ["HEC_TOKEN_AWS_SECRET_NAME"]),
+    index=os.environ["SPLUNK_INDEX"],
+    port=443,
+    proto="https",
+)
+logger = logging.getLogger("SplunkHecHandler")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(splunk_handler)
+
+
+def lambda_handler(event, context):
+    """
+    Entry point for lambda invocation via Eventbridge
+    """
+    print("Getting diagnostics...")
+
+    try:
+        start = time.time()
+        response = requests.get(os.environ["SENSITICS_URL"], timeout=5)
+        end = time.time()
+        request_timing_log = {"message": "Sensitics RTT", "RTT": end - start}
+        logger.debug(request_timing_log)
+        response.raise_for_status()
+        log_sensor_statuses(response.json()["sensors"])
+    except requests.exceptions.HTTPError as errh:
+        logger.error(f"Http Error: {errh}")
+        sys.exit()
+    except requests.exceptions.ConnectionError as errc:
+        logger.error(f"Error Connecting: {errc}")
+        sys.exit()
+    except requests.exceptions.Timeout as errt:
+        logger.error(f"Timeout Error: {errt}")
+        sys.exit()
+    except requests.exceptions.RequestException as err:
+        logger.error("Exception: {err}")
+        sys.exit()
+
+
+def log_sensor_statuses(sensors):
+    """
+    Helper method to iterate through sensor list and log to Splunk
+    """
+    print("Logging to Splunk...")
+    start = time.time()
+    for sensor_status in sensors:
+        sensor_status["message"] = "Sensor info"
+        logger.info(sensor_status)
+    end = time.time()
+
+    logger.debug(f"Logged to splunk in {end - start} seconds")
 
 
 if __name__ == "__main__":
